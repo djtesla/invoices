20426ed9717404877fab9a1e3db97807e6586233
djtesla <djtesla@gmailcom>
2021-09-24 20:52:45 +0200

    működik

--- src/main/resources/static/js/items-functions.js
+++ src/main/resources/static/js/items-functions.js
@@ -1,9 +1,29 @@
-function getItems(invoiceId) {
-    console.log(invoiceId);
-    //let listOfItemObj = []
+function getInvoiceSummary(invoice) {
+
+    let listOfItemObj = []
+
+    console.log(invoice[2]);
+
     let itemsTableBody = document.querySelector('#items-table-body');
     itemsTableBody.innerHTML = '';
 
+    let customerNameSpan = document.querySelector('#customer-name-span');
+    let issueDateSpan = document.querySelector('#issue-date-span');
+    let dueDateSpan = document.querySelector('#due-date-span');
+    let commentSpan = document.querySelector('#comment-span');
+    let invoiceTotalInHufSpan = document.querySelector('#invoice-total-in-huf-span');
+    let invoiceTotalInEurSpan = document.querySelector('#invoice-total-in-eur-span');
+
+    customerNameSpan.innerHTML = invoice[2];
+    issueDateSpan.innerHTML = invoice[3];
+    dueDateSpan.innerHTML = invoice[4];
+    issueDateSpan.textContent = issueDateSpan.textContent.replace(",", "-");
+    issueDateSpan.textContent = issueDateSpan.textContent.replace(",", "-");
+    dueDateSpan.textContent = dueDateSpan.textContent.replace(",", "-");
+    dueDateSpan.textContent = dueDateSpan.textContent.replace(",", "-");
+    commentSpan.innerHTML = invoice[5];
+
+    let invoiceId = invoice[0];
     let url = 'http://localhost:8080/api/invoices/' + invoiceId + '/items';
     fetch(url)
         .then(function (response) {
@@ -13,23 +33,29 @@ function getItems(invoiceId) {
                 console.log(jsonData);
                 let tableI = 0;
                 let totalItemPriceInEur = 0;
+                let totalHUF = 0;
+                let totalEUR = 0;
                 for (let item of jsonData) {
                     let cell;
                     let cellText;
                     tableI++;
                     let itemObj = [item.id, tableI, item.productName, item.unitPrice, item.quantity, item.totalItemPrice, totalItemPriceInEur]
-                    //listOfItemObj.push(itemObj);
+                    totalHUF += item.unitPrice * item.quantity;
+                    totalHUF+= totalItemPriceInEur;
+                    listOfItemObj.push(itemObj);
                     let row = document.createElement("tr");
                     itemObj[1] = tableI;
                     for (let i = 1; i < 7; i++) {
                         cell = document.createElement("td");
-                        cell.className="text-center";
+                        cell.className = "text-center";
                         cellText = document.createTextNode(itemObj[i]);
                         cell.appendChild(cellText);
                         row.appendChild(cell);
                     }
                     itemsTableBody.appendChild(row);
                 }
+            invoiceTotalInHufSpan.innerHTML = totalHUF;
+                invoiceTotalInEurSpan.innerHTML = totalEUR;
             }
         )
 }
@@ -39,13 +65,9 @@ function createItem(invoiceId) {
     let unitPriceInput = document.querySelector("#unit-price-input");
     let quantityInput = document.querySelector("#quantity-input");
 
-
-
     let productName = productNameInput.value;
     let unitPrice = unitPriceInput.value;
     let quantity = quantityInput.value;
-
-
 
     let data = {
         "productName": productName,
